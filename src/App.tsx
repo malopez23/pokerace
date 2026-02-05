83ae4a2d9641aec61057ca4b390053effe4d45cd
malopez23 <malopez.rodriguez23@gmail.com>
2026-02-04 21:03:32 -0300

    chore: complete project setup with Vite and Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-linear-to-br from-red-500 to-orange-500 flex items-center justify-center">
+      <h1 className="text-6xl font-black text-white hover:scale-110 transition">
+        Tailwind v4 is working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
